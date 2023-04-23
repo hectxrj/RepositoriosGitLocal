ed0cc3fa22b3546f56d98d546f6108d05249af18
Hector <Hector@DESKTOP-NSGQTH6.home>
2023-04-23 17:07:47 +0200

    Edicion by hector

--- proHectorHernandez/src/ramaHector/RamaHectorClase.java
+++ proHectorHernandez/src/ramaHector/RamaHectorClase.java
@@ -5,6 +5,7 @@ public class RamaHectorClase {
 	public static void main(String[] args) {
 		System.out.println("Mi primera clase Hector");
 		
+		System.out.println("edicion de la Rama by Hector");
 	}
 
 }
